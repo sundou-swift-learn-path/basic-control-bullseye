3b86c21ed123abb812cca082f91c49201908361f
XuBo <463467479@qq.com>
2019-01-08 14:07:47 +0800

    init

---
+++ bullsEye/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  bullsEye
+//
+//  Created by sundou on 2019/1/7.
+//  Copyright © 2019 sundou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert() {
+        print("Hello");
+    }
+}
+
