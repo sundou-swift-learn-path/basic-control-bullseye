5bd79cdad19202d0192995970b9410cc5475a75b
XuBo <463467479@qq.com>
2019-01-08 21:55:06 +0800

    IBOutlet

--- bullsEye/ViewController.swift
+++ bullsEye/ViewController.swift
@@ -9,14 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = Int(slider.value)
+        self.newRound()
     }
 
     @IBAction func showAlert() {
-        print("Hello");
+        let message = "The value of the slider is now: \(currentValue)" +
+        "\n The target value is: \(targetValue)"
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Tap", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMove(_ slider: UISlider) {
+        let roundValue = slider.value.rounded()
+        print("slider move value \(roundValue)")
+        
+        currentValue = Int(roundValue)
+    
+    }
+    
+    func newRound() {
+        targetValue = Int.random(in: 1...100)
+        slider.value = 50
     }
 }
 
